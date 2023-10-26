1cbca56b9d2b8356c792b3f6ff9ef7be2b14d184
Julien Bessac <ju.bessac@gmail.com>
2023-10-27 01:59:44 +0200

    working extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,31 @@
-import { window, commands, TextDocument, ExtensionContext } from "vscode";
+import { window, ExtensionContext } from "vscode";
 
 export function activate(context: ExtensionContext) {
   function deleteOtherSameTabs() {
-    let currentActiveTab = window.activeTextEditor?.document.fileName;
-    let tabGroups = window.tabGroups.all.map((tabGroup) =>
-      tabGroup.tabs.map((tab) => tab.label)
-    );
-    window.showInformationMessage(
-      currentActiveTab + "--------------------------" + tabGroups
-    );
+    let currentActiveFile = window.activeTextEditor?.document.fileName;
+    let currentTabGroup = window.tabGroups.activeTabGroup;
 
-    window.tabGroups.close(window.tabGroups.all[0]);
+    if (!currentActiveFile || !currentTabGroup) {
+      return;
+    }
+
+    window.tabGroups.all.forEach((tabGroup) =>
+      tabGroup.tabs.forEach((tab) => {
+        let tabInfos = tab.input as any;
+
+        if (
+          tabInfos.uri.path === currentActiveFile &&
+          tabGroup !== currentTabGroup
+        ) {
+          window.tabGroups.close(tab);
+        }
+      })
+    );
   }
 
-  let listener = window.onDidChangeActiveTextEditor((e) =>
+  let listener = window.onDidChangeVisibleTextEditors(() =>
     deleteOtherSameTabs()
   );
 
-  let disposable = commands.registerCommand(
-    "auto-close-duplicate-file.helloWorld",
-    () => {
-      window.showInformationMessage(
-        "This is the current active editor" +
-          window.activeTextEditor?.document.fileName +
-          "/// All opened tabs\n" +
-          window.tabGroups.all.map(
-            (tabGroup, index) =>
-              index.toString() + tabGroup.tabs.map((tab) => tab.label)
-          )
-      );
-    }
-  );
-
   context.subscriptions.push(listener);
-  context.subscriptions.push(disposable);
 }
