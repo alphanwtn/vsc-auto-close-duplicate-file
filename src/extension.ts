71fc12cf9bb3878d6b2d28ca3ecc467f8de0a559
Julien Bessac <ju.bessac@gmail.com>
2023-10-27 19:51:09 +0200

    working single tab case

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,66 @@
-import { window, ExtensionContext, TabInputCustom, TabInputText } from "vscode";
+import {
+  window,
+  ExtensionContext,
+  TabInputText,
+  workspace,
+  ConfigurationTarget,
+} from "vscode";
 
 export function activate(context: ExtensionContext) {
-  function deleteOtherSameTabs() {
-    let activeFile = window.activeTextEditor?.document.fileName;
+  function deleteIdenticalTabs() {
+    const tempListener = context.subscriptions.pop();
+    let activeFile = window.activeTextEditor?.document;
     let activeTabGroup = window.tabGroups.activeTabGroup;
 
-    if (!activeFile || !activeTabGroup) {
-      return;
+    function handleSingleLastTab() {
+      workspace
+        .getConfiguration("workbench.editor")
+        .update("revealIfOpen", true, ConfigurationTarget.Global);
+
+      if (!activeTabGroup.activeTab || !activeFile) {
+        return;
+      }
+
+      window.tabGroups.close(activeTabGroup.activeTab);
+
+      window.showTextDocument(activeFile);
+
+      workspace
+        .getConfiguration("workbench.editor")
+        .update("revealIfOpen", false, ConfigurationTarget.Global);
     }
 
     window.tabGroups.all.forEach((tabGroup) =>
       tabGroup.tabs.forEach((tab) => {
-        let tabInfos = tab.input as TabInputText;
+        let tabInput = tab.input as TabInputText;
 
-        if (tabInfos.uri.path === activeFile && tabGroup !== activeTabGroup) {
-          window.tabGroups.close(tab);
+        if (!activeFile || !activeTabGroup) {
+          return;
+        }
+
+        if (
+          tabInput.uri.path === activeFile.fileName &&
+          tabGroup !== activeTabGroup
+        ) {
+          if (tabGroup.tabs.length > 1) {
+            return window.tabGroups.close(tab);
+          } else {
+            return handleSingleLastTab();
+          }
         }
       })
     );
+    context.subscriptions.push(tempListener!);
   }
 
-  let listener = window.onDidChangeVisibleTextEditors(() =>
-    deleteOtherSameTabs()
-  );
+  workspace
+    .getConfiguration("workbench.editor")
+    .update("revealIfOpen", false, ConfigurationTarget.Global);
+
+  let listener = window.onDidChangeVisibleTextEditors((e) => {
+    window.showErrorMessage("Cycle");
+    deleteIdenticalTabs();
+  });
 
   context.subscriptions.push(listener);
 }
